ea6cfcccef1db7708620ed1d41b023e232bef8e9
Shone <your-email@example.com>
2026-02-05 15:41:23 +0530

    fix in build.sh backend folder

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+echo "📦 Installing dependencies..."
+pip install -r requirements.txt
+
+echo "📁 Moving into backend folder..."
+
+
+echo "🗄 Running migrations..."
+python manage.py migrate
+
+echo "📦 Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "✅ Build completed"
